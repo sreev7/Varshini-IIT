8c13fbf9496573d445341d3eddd3cb9efe2c0ae8
Varshini <105491861+sreev7@users.noreply.github.com>
2022-05-25 21:53:36 +0530

    ClassesandObjects-CustomerClass

---
+++ Customer.java
@@ -0,0 +1,22 @@
+
+
+class Customer1{
+		String fullName;
+		int age;
+	}
+
+public class Customer {
+
+		public static void main(String[] args)
+		{
+			// TODO Auto-generated method stub
+	       Customer1 customer = new Customer1();
+	       customer.age = 24;
+	       customer.fullName = "Seeta";
+	       
+	       System.out.println(customer.fullName);	
+	       System.out.println(customer.age);
+	       
+		}	
+
+}
